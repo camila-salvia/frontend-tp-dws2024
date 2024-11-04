462ff6a60d06d5a124d0eecbd304e1652275c18b
Camila Salvia <camilasalvia2@gmail.com>
2024-11-04 13:53:58 -0300

    se muestran todas las canchas OK, sin filtrado

--- src/app/components/ventana-home/ventana-home.component.ts
+++ src/app/components/ventana-home/ventana-home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ventana-home',
   standalone: true,
   imports: [
-    RouterModule // Recordar agregar siempre!!
+    RouterModule, // Recordar agregar siempre!!
+    CommonModule,
   ],
   templateUrl: './ventana-home.component.html',
   styleUrl: './ventana-home.component.css'
--- src/app/components/ventana-reservas/ventana-reservas.component.ts
+++ src/app/components/ventana-reservas/ventana-reservas.component.ts
@@ -2,41 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { CANCHA_DATA } from '../../../assets/canchas.js';
 import { NCancha } from '../../models/cancha.models.js';
 import { CanchasComponent } from '../canchas/canchas.component.js';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-ventana-reservas',
   standalone: true,
   imports: [
-    CanchasComponent
+    CanchasComponent,
+    RouterModule, // Recordar agregar siempre!!
+    CommonModule,
   ],
   templateUrl: './ventana-reservas.component.html',
   styleUrl: './ventana-reservas.component.css'
 })
 
-export class VentanaReservasComponent{
-  ngOnInit() {
-  console.log(this.datosCancha); // Esto debería mostrar el array de canchas en la consola
-}
-  //canchasFutbol7 = [];
-  //canchasFutbol11 = [];
-
-  datosCancha = CANCHA_DATA;
-  getCanchaInfo(val: NCancha.CanchaData){
+export class VentanaReservasComponent implements OnInit{
+  datosCancha: NCancha.CanchaData[] = CANCHA_DATA;
+  ngOnInit(): void {
+    console.log(this.datosCancha);  // Verifica que CANCHA_DATA contiene los datos esperados --> Los tiene
+  }
+  getCanchaInfo(val: NCancha.CanchaData): void {
     console.log(val);
   }
   trackByFn(_index: number, item:NCancha.CanchaData){ //solo se renderiza el elemento modificado
     return item.id;
   }
-}
-
-
-  /*
-  ngOnInit() {
-    this.cargarCanchasPorTipo();
-  }
-
-  cargarCanchasPorTipo() {
-    this.canchasFutbol7 = CANCHA_DATA.filter(c => c.tipo === 'futbol 7');
-    this.canchasFutbol11 = CANCHA_DATA.filter(c => c.tipo === 'futbol 11');
+/*
+  datosCancha = CANCHA_DATA;
+  getCanchaInfo(val: NCancha.CanchaData){
+    console.log(val);
   }
 */
+}
