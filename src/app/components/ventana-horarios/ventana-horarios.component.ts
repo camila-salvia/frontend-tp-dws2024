04a0262f53c0c44263dcef3b18f3e0c282765f67
Martinvivas <martinvivas852@gmail.com>
2024-11-06 22:22:30 -0300

    intento de fecha

--- src/app/components/ventana-horarios/ventana-horarios.component.ts
+++ src/app/components/ventana-horarios/ventana-horarios.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Agregar FormsModule
 
 @Component({
   selector: 'app-ventana-horarios',
   standalone: true,
   imports: [
-    RouterModule, // Recordar agregar siempre!!
+    RouterModule,
     CommonModule,
+    FormsModule, // Importar FormsModule aquí
   ],
   templateUrl: './ventana-horarios.component.html',
-  styleUrl: './ventana-horarios.component.css'
+  styleUrls: ['./ventana-horarios.component.css'],
 })
 export class VentanaHorariosComponent {
+  fechaSeleccionada: string | null = null;
+  horaSeleccionada: string | null = null;
+  botonHabilitado: boolean = false;
 
+  verificarSeleccion() {
+    // El botón se habilita solo si tanto la fecha como la hora están seleccionadas
+    this.botonHabilitado = !!this.fechaSeleccionada && !!this.horaSeleccionada;
+  }
 }
