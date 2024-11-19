7a74b47abb6206467513c6efdc300d1d6169898a
OctavioSesana <osesana@frro.utn.edu.ar>
2024-11-18 21:03:08 -0300

    mis reservas -- actualizado
    detecta que hay reservas cargadas pero no muestra sus datos

--- src/app/components/ventana-mis-reservas/ventana-mis-reservas.component.ts
+++ src/app/components/ventana-mis-reservas/ventana-mis-reservas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { Reserva } from '../../models/lista-reservas.models';
 
 @Component({
   selector: 'app-ventana-mis-reservas',
@@ -11,6 +12,30 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './ventana-mis-reservas.component.html',
   styleUrls: ['./ventana-mis-reservas.component.css'],
 })
+export class VentanaMisReservasComponent {
+  @Input() resrevaData!: Reserva;
+  //@Output() onClickIcon = new EventEmitter<NCancha.CanchaData>();
+  reservas: Reserva[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.mostrarReservas();
+  }
+
+  mostrarReservas(): void {
+    this.apiService.getReservas().subscribe({
+      next: (data) => {
+        this.reservas = data;
+      },
+      error: (error) => {
+        console.error('Error al obtener las reservas', error);
+      },
+    });
+  }
+}
+
+/*
 export class VentanaMisReservasComponent implements OnInit {
   reservas: any[] = [];
 
@@ -20,63 +45,15 @@ export class VentanaMisReservasComponent implements OnInit {
     this.mostrarReservas();
   }
 
-  /* mostrarReservas(): void {
-    this.apiService
-      .getReservas()
-      .subscribe(
-        (data) => {
-          this.reservas = data;
-        },
-        (error) => {
-          console.error('Error al obtener las reservas', error);
-        }
-      );
-  }
-} */
-
   mostrarReservas(): void {
-    this.reservas = [
-      this.apiService
-        .getReservas(/*'http://localhost:3000/api/reserva'*/)
-        .subscribe(
-          (data) => {
-            this.reservas = data;
-          },
-          (error) => {
-            console.error('Error al obtener las reservas', error);
-          }
-        ),
-      /*
-      {
-        cancha: 'Cancha 1',
-        fecha: '2021-10-10',
-        hora: '10:00',
-        duracion: '1 hora',
-      },
-      {
-        cancha: 'Cancha 2',
-        fecha: '2021-10-10',
-        hora: '11:00',
-        duracion: '1 hora',
-      },
-      {
-        cancha: 'Cancha 3',
-        fecha: '2021-10-10',
-        hora: '12:00',
-        duracion: '1 hora',
-      },
-      */
-    ];
-  }
-}
-
-/* mostrarReservas(): void {
-    this.apiService.getReservas().subscribe(
-      (data) => {
+    this.apiService.getReservas().subscribe({
+      next: (data) => {
         this.reservas = data;
       },
-      (error) => {
+      error: (error) => {
         console.error('Error al obtener las reservas', error);
-      }
-    );
-  } */
+      },
+    });
+  }
+}
+*/
